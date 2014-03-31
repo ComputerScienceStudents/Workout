e02d51c4e2146ddba4f055dda2aad26834deee67
Michal Dul <mdul@student.agh.edu.pl>
2014-04-01 01:06:30 +0200

    model and backend controller for exercise

---
+++ app/models/exercise.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Exercise Schema
+ */
+var ExerciseSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    video: {
+        type: String,
+        default: '',
+        trim: true
+    }
+});
+
+/**
+ * Validations
+ */
+ExerciseSchema.path('title').validate(function(title) {
+    return title.length;
+}, 'Title cannot be blank');
