8b273cd7c02d367e1e36f0e5c9c1e207493c4927
michal <michaldul@outlook.com>
2014-04-08 08:32:38 +0200

    predefined exercises goes first

--- app/controllers/exercises.js
+++ app/controllers/exercises.js
@@ -95,16 +95,18 @@ exports.all = function(req, res) {
             });
         } else {
             // fake exercises
-            exercises.push(new Exercise({title: 'push-ups', description: 'A push-up is a common calisthenics exercise performed in a prone position by raising and lowering the body using the arms. Push-ups exercise the pectoral muscles, triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, serratus anterior, coracobrachialis and the midsection as a whole.',
+            var predefined = [];
+
+            predefined.push(new Exercise({title: 'push-ups', description: 'A push-up is a common calisthenics exercise performed in a prone position by raising and lowering the body using the arms. Push-ups exercise the pectoral muscles, triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, serratus anterior, coracobrachialis and the midsection as a whole.',
                 video: 'http://www.youtube.com/watch?v=FtcKhNzopTo', minature: 'http://i1.ytimg.com/vi/FtcKhNzopTo/mqdefault.jpg'}));
-            exercises.push(new Exercise({title: 'pull-ups', description: 'A pull-up is a variety of upper-body compound pulling motions for the purpose of exercise. The pull-up has two main methods of execution; the first is with a pronated (overhand) grip and the second is with a supinated (underhand) grip.',
+            predefined.push(new Exercise({title: 'pull-ups', description: 'A pull-up is a variety of upper-body compound pulling motions for the purpose of exercise. The pull-up has two main methods of execution; the first is with a pronated (overhand) grip and the second is with a supinated (underhand) grip.',
                 video: 'http://www.youtube.com/watch?v=blpuO2vJo38', minature: 'http://i1.ytimg.com/vi/blpuO2vJo38/mqdefault.jpg'}));
-            exercises.push(new Exercise({title: 'squats', description: 'In strength training, the squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips and buttocks, quads (vastus lateralus medialis and intermedius), hamstrings, as well as strengthening the bones, ligaments and insertion of the tendons throughout the lower body.',
+            predefined.push(new Exercise({title: 'squats', description: 'In strength training, the squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips and buttocks, quads (vastus lateralus medialis and intermedius), hamstrings, as well as strengthening the bones, ligaments and insertion of the tendons throughout the lower body.',
                 video: 'http://www.youtube.com/watch?v=xK9jzjsTJts', minature: 'http://i1.ytimg.com/vi/xK9jzjsTJts/mqdefault.jpg'}));            
-            exercises.push(new Exercise({title: 'stretching', description: 'Stretching is a form of physical exercise in which a specific muscle or tendon (or muscle group) is deliberately flexed or stretched in order to improve the muscles felt elasticity and achieve comfortable muscle tone. The result is a feeling of increased muscle control, flexibility and range of motion. Stretching is also used therapeutically to alleviate cramps.',
+            predefined.push(new Exercise({title: 'stretching', description: 'Stretching is a form of physical exercise in which a specific muscle or tendon (or muscle group) is deliberately flexed or stretched in order to improve the muscles felt elasticity and achieve comfortable muscle tone. The result is a feeling of increased muscle control, flexibility and range of motion. Stretching is also used therapeutically to alleviate cramps.',
                 video: 'http://www.youtube.com/watch?v=sVZBZ6wpqmg', minature: 'http://i1.ytimg.com/vi_webp/sVZBZ6wpqmg/mqdefault.webp'}));
             
-            res.jsonp(exercises);
+            res.jsonp(predefined.concat(exercises));
         }
     });
 };
