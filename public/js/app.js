8f4357a2ddd7de27bf87aa0d4a055f34952cdd5d
Piotr Podolski <9g9man@gmail.com>
2014-04-27 17:54:23 +0200

    workout mode!g

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('workout', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'workout.system', 'workout.exercises', 'workout.programs', 'workout.timer']);
+angular.module('workout', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'workout.system', 'workout.exercises', 'workout.programs', 'workout.timer', 'workout.workoutMode']);
 
 angular.module('workout.system', []);
 angular.module('workout.exercises', ['ui.select2']);
 angular.module('workout.programs', ['ui.bootstrap']);
+angular.module('workout.workoutMode', ['ui.bootstrap']);
