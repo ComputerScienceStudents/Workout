e1bf9b2fbe6273a49caa0194f1a7d902f47e8534
Jakub Mazurek <jmazurek@student.agh.edu.pl>
2014-04-29 08:51:07 +0200

    [68068010] changes after review

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,39 +1,47 @@
 angular.module('workout.timer', [])
   .directive('timer', [
+
     function() {
       return {
         replace: true,
-        template:"<div class='timer'><span class='label'>Time left: </span><span class='time {{timerColor()}}' ng-class='{hidden:!isVisible}'>{{timeLeft}}</span></div>",
+        template: "<div class='timer'>" +
+          "<span class='label'>Time left: </span>" +
+          "<span class='time {{timerColor()}}' ng-class='{hidden:!isVisible}'>{{timeLeft}}</span>" +
+          "</div>",
         scope: {
           countdownTime: '='
         },
         controller: ['$scope', '$element', '$attrs', '$interval',
           function($scope, $element, $attrs, $interval) {
-            $scope.blink = function(){
-              $interval(function(){
+            
+            $scope.blink = function() {
+              $interval(function() {
                 $scope.isVisible = !$scope.isVisible;
               }, 100);
             }
+            
             $scope.on = function() {
               var iPromise = $interval(function() {
                 $scope.timeLeft -= 1;
-                if($scope.timeLeft<=0){
+                if ($scope.timeLeft <= 0) {
                   $interval.cancel(iPromise);
                   $scope.blink();
                 }
               }, 1000, $scope.on);
+
             }
           }
         ],
-        link: function($scope,$element,$attrs){
+        link: function($scope, $element, $attrs) {
           $scope.timeLeft = $scope.countdownTime;
           $scope.isVisible = true;
-          $scope.timerColor = function(){
-            if($scope.timeLeft/$scope.countdownTime>0.5){
+          
+          $scope.timerColor = function() {
+            if ($scope.timeLeft / $scope.countdownTime > 0.5) {
               return "green";
-            }else if($scope.timeLeft/$scope.countdownTime>0.3){
+            } else if ($scope.timeLeft / $scope.countdownTime > 0.3) {
               return "yellow";
-            }else{
+            } else {
               return "red";
             }
           }
