fb8b95adbbf6fa27b50f7fdb0123ef6f9551c2f5
Puszek_SE <puszekse@gmail.com>
2014-05-06 23:32:06 +0200

    adding programscreation controller

---
+++ public/js/controllers/programscreation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('workout.programscreation').controller('ProgramsController', ['$scope', '$stateParams', '$location', 'Global', 'Programs', function ($scope, $stateParams, $location, Global, Programs) {
+    $scope.global = Global;
+
+
+     $scope.create = function() {
+        var program = new Program({
+            title: this.title,
+            description: this.description,
+            lead: this.lead
+        });
+
+        program.$save(function(response) {
+            $location.path('programs/' + response._id);
+        });
+
+        this.title = '';
+        this.description = '';
+        this.lead = '';
+    };
+}]);
