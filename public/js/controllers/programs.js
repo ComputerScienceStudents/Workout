66f68c260bffc9a817075d6b924fd8b0b5ecbcca
Puszek_SE <puszekse@gmail.com>
2014-05-11 21:45:23 +0200

    Unlinked 'add exercise to program'
    Solution not linked to actual buttons

--- public/js/controllers/programs.js
+++ public/js/controllers/programs.js
@@ -2,7 +2,7 @@
 
 angular.module('workout.programs').controller('ProgramsController', ['$scope', '$stateParams', '$location', 'Global', 'Programs','Exercises', function ($scope, $stateParams, $location, Global, Programs, Exercises) {
     
-    $scope.exercises = []
+    $scope.exercises = [];
 
     $scope.create = function() {
         var program = new Programs({
@@ -19,19 +19,24 @@ angular.module('workout.programs').controller('ProgramsController', ['$scope', '
         this.title = '';
         this.description = '';
         this.lead = '';
+        $scope.exercises = [];
     };
 
 
 
     $scope.addexercise = function(){
         var exId = db.exercises.findOne({title:this.exercise_title})._id;
+        if (!exId) {
+            return next(new Error('Failed to load exercise: ' + exercise_title));
+        }else{
+            var exercises = $scope.exercises;
+            exercises.push({
+                repetitions: this.repetitions,
+                pause: this.pause,
+                exercise: exId});
+            $scope.exercises = exercises;
 
-        var exercises = $scope.exercises
-        exercises.push({
-        repetitions: this.repetitions,
-        pause: this.pause,
-        exercise: exId
-    })
+        }
     }
 
     $scope.remove = function(){
