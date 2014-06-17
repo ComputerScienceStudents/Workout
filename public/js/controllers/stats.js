262c7a29ac317edf7927cba2bd5877f2a3564df0
michal <michaldul@outlook.com>
2014-06-17 02:00:23 +0200

    two types of charts

--- public/js/controllers/stats.js
+++ public/js/controllers/stats.js
@@ -4,6 +4,14 @@ angular.module('workout.stats').controller('StatsController', ['$scope', '$state
     
     var username = Global.user.username;
 
+    // nobody will see
+    function getWeekNumber(date){
+        var d = new Date(+date);
+        d.setHours(0,0,0);
+        d.setDate(d.getDate()+4-(d.getDay()||7));
+        return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+    };
+
     function mapStatsHistory(workouts) {
         var stats = {};
         for (var i = 0; i < workouts.length; i++) {
@@ -14,7 +22,7 @@ angular.module('workout.stats').controller('StatsController', ['$scope', '$state
 
     function mapWorkoutStats(workout, stats) {
         for (var i = 0; i < workout.exercises.length; i++) {
-            var exercise = mapExerciseName(workout.exercises[i]._id);
+            var exercise = mapExerciseName(workout.exercises[i].exercise);
             if(!(exercise in stats)) {
                 stats[exercise] = {};
             }
@@ -26,192 +34,131 @@ angular.module('workout.stats').controller('StatsController', ['$scope', '$state
         return $scope.exercises.filter(function(e) { return e._id === id; })[0].title;
     }
 
+    function mapToCharst(stats, chartConfig) {
+        var mapped = []
+        for(var exercise in stats) {
+            mapped.push(chartConfig(stats[exercise], exercise));
+        }
+        return mapped;
+    }
+
+    function avg(numbers) {
+        var sum = 0;
+        for (var i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        };
+        return sum/numbers.length;
+    }
+
+    function chartConfig(stats, exerciseName) {
+        var data = [];
+        for(var date in stats) {
+            data.push([Date.parse(date), stats[date]]);
+        }
+
+        var period = ' from ' + data[0][0] + ' to ' + data[data.length - 1][0];
+
+        return {
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: exerciseName
+            },
+            subtitle: {
+                text: exerciseName + period
+            },
+            xAxis: {
+                type: 'datetime',
+                    dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                },
+                title: {
+                    text: 'Date'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of repetitions'
+                },
+                min: 0
+            },
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+            },
+
+            series: [{
+                name: exerciseName,
+                data: data
+                }]
+            };
+    }
+
+    function weekChartConfig(stats, exerciseName) {
+        var data = {};
+        for(var date in stats) {
+            var week = getWeekNumber(Date.parse(date));
+            if(!(week in data)) {
+                data[week] = [];
+            }
+            data[week].push(stats[date]);
+        }
+        var chartData = [];
+        for(var week in data) {
+            chartData.push([week, avg(data[week])])
+        }
+
+
+        var period = ' from ';// + data[0][0] + ' to ' + data[data.length - 1][0];
+
+        return {
+            chart: {
+                type: 'column',
+            },
+            title: {
+                text: exerciseName
+            },
+            subtitle: {
+                text: exerciseName + period
+            },
+            xAxis: {
+                type: 'datetime',
+                    dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                },
+                title: {
+                    text: 'Date'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'average number of repetitions'
+                },
+                min: 0
+            },
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+            },
+
+            series: [{
+                type: 'column',
+                name: exerciseName,
+                data: chartData
+            }]
+        };
+    }
+
     $scope.initialize = function() {
         Exercises.query(function(exercises) {   
             $scope.exercises = exercises;         
             Stats.query(function(stats) {
-                $scope.stats = mapStatsHistory(stats);
+                $scope.chartsConfig = mapToCharst(mapStatsHistory(stats), chartConfig);
+                $scope.weeklyCharts = mapToCharst(mapStatsHistory(stats), weekChartConfig);
             });
         });
     }
-
-    $scope.chart0 = {
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Push-ups'
-        },
-        subtitle: {
-            text: 'Push-ups from 27.09.1970 to 12.05.1971'
-        },
-        xAxis: {
-            type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                month: '%e. %b',
-                year: '%b'
-            },
-            title: {
-                text: 'Date'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Number of repetitions'
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-        },
-
-        series: [{
-            name: 'Push-ups',
-                // Define the data points. All series have a dummy year
-                // of 1970/71 in order to be compared on the same x axis. Note
-                // that in JavaScript, months start at 0 for January, 1 for February etc.
-                data: [
-                [Date.UTC(1970,  9, 27), 3   ],
-                [Date.UTC(1970, 10, 10), 6 ],
-                [Date.UTC(1970, 10, 18), 7 ],
-                [Date.UTC(1970, 11,  2), 8 ],
-                [Date.UTC(1970, 11,  9), 6 ],
-                [Date.UTC(1970, 11, 16), 6 ],
-                [Date.UTC(1970, 11, 28), 7],
-                [Date.UTC(1971,  0,  1), 8],
-                [Date.UTC(1971,  0,  8), 8],
-                [Date.UTC(1971,  0, 12), 8],
-                [Date.UTC(1971,  0, 27), 8],
-                [Date.UTC(1971,  1, 10), 8],
-                [Date.UTC(1971,  1, 18), 8],
-                [Date.UTC(1971,  1, 24), 9],
-                [Date.UTC(1971,  2,  4), 9],
-                [Date.UTC(1971,  2, 11), 9],
-                [Date.UTC(1971,  2, 15), 10],
-                [Date.UTC(1971,  2, 25), 12],
-                [Date.UTC(1971,  3,  2), 13],
-                [Date.UTC(1971,  3,  6), 15],
-                [Date.UTC(1971,  3, 13), 16 ],
-                [Date.UTC(1971,  4,  3), 20 ],
-                [Date.UTC(1971,  4, 26), 18 ],
-                [Date.UTC(1971,  5,  9), 22],
-                [Date.UTC(1971,  5, 12), 24 ]
-                ]
-            }]
-        },
-
-    $scope.chart1 = {
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Pull-ups'
-        },
-        subtitle: {
-            text: 'Pull-ups from 18.09.1970 to 07.05.1971'
-        },
-        xAxis: {
-            type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                month: '%e. %b',
-                year: '%b'
-            },
-            title: {
-                text: 'Date'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Number of repetitions'
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-        },
-
-        series: [{
-                name: 'Pull-ups',
-                data: [
-                [Date.UTC(1970,  9, 18), 3   ],
-                [Date.UTC(1970,  9, 26), 2 ],
-                [Date.UTC(1970, 11,  1), 4],
-                [Date.UTC(1970, 11, 11), 5],
-                [Date.UTC(1970, 11, 25), 8],
-                [Date.UTC(1971,  0,  8), 8],
-                [Date.UTC(1971,  0, 15), 9],
-                [Date.UTC(1971,  1,  1), 8],
-                [Date.UTC(1971,  1,  8), 6],
-                [Date.UTC(1971,  1, 21), 5 ],
-                [Date.UTC(1971,  2, 12), 9],
-                [Date.UTC(1971,  2, 25), 10 ],
-                [Date.UTC(1971,  3,  4), 9],
-                [Date.UTC(1971,  3,  9), 9],
-                [Date.UTC(1971,  3, 13), 11],
-                [Date.UTC(1971,  3, 19), 12 ],
-                [Date.UTC(1971,  4, 25), 12 ],
-                [Date.UTC(1971,  4, 31), 15],
-                [Date.UTC(1971,  5,  7), 14 ]
-                ]
-            }]
-        },
-
-    $scope.chart2 = {
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Squats'
-        },
-        subtitle: {
-            text: 'Squats from 18.09.1970 to 07.05.1971'
-        },
-        xAxis: {
-            type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                month: '%e. %b',
-                year: '%b'
-            },
-            title: {
-                text: 'Date'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Number of repetitions'
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-        },
-
-        series: [{
-                name: 'Squats',
-                data: [
-                [Date.UTC(1970,  9, 18), 20   ],
-                [Date.UTC(1970,  9, 26), 22 ],
-                [Date.UTC(1970, 11,  1), 24 ],
-                [Date.UTC(1970, 11, 11), 26 ],
-                [Date.UTC(1970, 11, 25), 30],
-                [Date.UTC(1971,  0,  8), 38],
-                [Date.UTC(1971,  0, 15), 38],
-                [Date.UTC(1971,  1,  1), 38],
-                [Date.UTC(1971,  1,  8), 48],
-                [Date.UTC(1971,  1, 21), 50 ],
-                [Date.UTC(1971,  2, 12), 40 ],
-                [Date.UTC(1971,  2, 25), 44 ],
-                [Date.UTC(1971,  3,  4), 52 ],
-                [Date.UTC(1971,  3,  9), 61],
-                [Date.UTC(1971,  3, 13), 50],
-                [Date.UTC(1971,  3, 19), 40 ],
-                [Date.UTC(1971,  4, 25), 30 ],
-                [Date.UTC(1971,  4, 31), 35],
-                [Date.UTC(1971,  5,  7), 20   ]
-                ]
-            }]
-        }
-    }]);
+}]);
